78c53bf7406e9f0958805be14bf8d8b827690418
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 12:37:16 +0100

    Exercise 03

---
+++ ex03/index.js
@@ -0,0 +1,9 @@
+var myPetsArray  = ["Dog", "Cat"];
+myPetsArray.pop["Bird", "Fish"];
+function myArrayFunction(myPets) {
+    var myNewPets = [...myPets];
+    return myPetsArray;
+}
+myArrayFunction(myNewPets);
+console.log(myArrayFunction(myNewPets));
+module.exports = myArrayFunction;
