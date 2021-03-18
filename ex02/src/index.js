4652cb52453d28dcc3946c88ad7e43985c8300ee
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 12:26:12 +0100

    origin master

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+var temp  = [
+    [31, 32, 19, 37],
+    [29, 27, 55, 36],
+    [17, 27, 42, 46],
+    [29, 52, 21, 64],
+    [91, 27, 31, 61]
+];
+function myArrayFunction(arr) {
+    var newTemp = [...arr];
+    var averageDayTemp = [];
+
+   
+    return averageDayTemp;
+}
+myArrayFunction(averageDayTemp);
+console.log(myArrayFunction(averageDayTemp));
+module.exports = myArrayFunction;
