ce583cb554d16551adfee1167b87d4a846fffe4f
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 12:10:57 +0100

    Exercise 01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,15 +1,9 @@
-// Create an array here
 var myArr = [2, 4, 0, 8, 10];
-// End of creating an array
-
 function myArrayFunction(arr) {
     var myItems = [...arr];
-    // Only change code below this line
     myItems[2] = 6;
-    // Only change code below this line
     return myItems;
-
 }
-
-console.log(myArrayFunction(myArr)); // Change this line
+myArrayFunction(myArr);
+console.log(myArrayFunction(myArr));
 module.exports = myArrayFunction;
