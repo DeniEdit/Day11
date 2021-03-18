b9126209e52eddcee47ae1673d9794912b1a0292
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 12:45:50 +0100

    Exercise 04

---
+++ ex04/src/index.js
@@ -0,0 +1,8 @@
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+var monitorsList = [];
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+}
+myMonitorsFunction();
+console.log(myMonitorsFunction());
+module.exports = myMonitorsFunction;
