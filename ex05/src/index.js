f213b166f032ae60f5eb120c497d3c601d0570bb
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 13:30:28 +0100

    Exercise 05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,6 +2,6 @@ var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
 function myNestedFunction(arr){
 return myNestedArray;
 }
-myNestedFunction(arr);
-console.log(myNestedFunction(arr));
+myNestedFunction();
+console.log(myNestedFunction());
 module.exports = myNestedFunction;
