195529b676ffa6e9c3d7ed8f833ec72546cce07c
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 22:32:44 +0100

    Exercise 09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,4 +1,11 @@
 function myBouncer(arr) {
+    var newArr = [];
+    for (i =0; i< arr.length; i++) {
+        if(arr[i] == true){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 }
-console.log(myBouncer());
+console.log(myBouncer([7, "ate", "", false, 9]));
 module.exports = myBouncer;
