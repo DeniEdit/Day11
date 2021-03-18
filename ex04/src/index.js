06a08f6066adca7817f0d117dae3ab357f200dcc
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 21:26:10 +0100

    Exercise 04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -2,7 +2,15 @@ var monitorsListArray = ["Apple", "Peach", "Berry"];
 var monitorsList = [];
 function myMonitorsFunction(arr) {
     var newMonitorsList = [...arr];
+    newMonitorsList = monitorsList;
+    var i;
+    for (i = 0; i<3; i++){
+        monitorsList.push([monitorsListArray[i]] + "," + i);
+    }
+    return monitorsList;
+
 }
-myMonitorsFunction();
-console.log(myMonitorsFunction());
+myMonitorsFunction(monitorsListArray);
+console.log(myMonitorsFunction(monitorsListArray));
 module.exports = myMonitorsFunction;
+
