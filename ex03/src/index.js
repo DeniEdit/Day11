920bbda94d400ef0b2bc4a0722cbdd3f32655f15
DeniEdit <milicevic.deni@gmail.com>
2021-03-18 21:05:14 +0100

    Exercise 03

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,12 +1,14 @@
 var myPetsArray  = ["Dog", "Cat"];
 function myArrayFunction(myPets) {
     var myNewPets = [...myPets];
-    for (i = 0; i < myPets; i++)
-    {
+    var firstPet = [];
+
+    for (i=0; i<arr.length; i++) {
         myNewPets = myPets[i];
     }
-    return myPetsArray;
-}
+    myNewPets.unshift("Bird", "Fish");
+    return firstPet;
+} 
 myArrayFunction(myPetsArray);
 console.log(myArrayFunction(myPetsArray));
 module.exports = myArrayFunction;
